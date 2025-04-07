6612b59aa38402e727cd7208c92e94acdf0686fd
ShreedharRamnarain <83304101+Shreedhar204@users.noreply.github.com>
2025-04-07 21:22:18 +0200

    added geolocation service

---
+++ angular-weather-app/src/app/services/geolocation.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GeolocationService {
+  getCurrentPosition(): Observable<any> {
+    return new Observable((observer) => {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            console.log('Geolocation success:');
+            console.log('Latitude:', position.coords.latitude);
+            console.log('Longitude:', position.coords.longitude);
+            console.log('Accuracy (meters):', position.coords.accuracy);
+            observer.next(position);
+            observer.complete();
+          },
+          (error) => {
+            observer.error(error);
+          }
+        );
+      } else {
+        observer.error('Geolocation is not available in this browser.');
+      }
+    });
+  }
+}
